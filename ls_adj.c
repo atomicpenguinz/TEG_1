4545e1e83fd1871c10bc0d4e3193aa6cd3311c72
atomicpenguinz <heitorlcaetano@gmail.com>
2026-05-01 11:52:12 -0300

    removendo lixo

--- ls_adj.c
+++ ls_adj.c
@@ -11,37 +11,8 @@ Grafo *cria_grafo(uint tam) {
 	}
 
 	novo->count = tam;
-	// novo->capacity = tam;
 	return novo;
 }
-
-// void add_vertice(Grafo *g, Nodo *elemento) {
-// 	if(g->count >= g->capacity) {
-// 		g->capacity = g->capacity == 0 ? 4 : g->capacity * 2;
-// 		g->array = realloc(g->array, sizeof(Nodo*) * g->capacity);
-// 	}
-// 	g->array[g->count++] = elemento;
-// }
-
-// void garante_capacidade(Grafo *g, uint elemento) {
-// 	uint index = elemento;
-// 	if(!g->capacity)
-// 		g->capacity = 4;
-// 	if(index >= g->capacity) {
-// 		uint old_cpct= g->capacity;
-// 		while(index >= g->capacity) {
-// 			g->capacity *= 2;
-// 		}
-// 		Nodo **temp = realloc(g->array, sizeof(Nodo*) * g->capacity);
-// 		if(!temp) {
-// 			free_grafo(g);
-// 			return;
-// 		}
-// 		g->array = temp;
-// 		for(uint i = old_cpct; i < g->capacity; i++)
-// 			g->array[i] = NULL;
-// 	}
-// }
 
 static void free_lista(Nodo *head) {
 	if(!head) return;
@@ -73,34 +44,6 @@ void add_nodo(Grafo *g, uint novo, uint index) {
 	new->prox = g->array[index];
 	g->array[index] = new;
 }
-
-// static Nodo *mergesort_list(Nodo *first) {
-// 	Nodo *aux = first;
-// 	for(Nodo *fim = first; fim != NULL; fim = fim->prox->prox, aux = aux->prox);
-
-// 	Nodo *lista2 = aux->prox;
-// 	aux->prox = NULL;
-	
-// 	returnçNULL;
-// ç
-
-// static uiçt duplicados_aux(Nodo *atual, uint acc) { // tail call recursion
-// 	if(!atual || !(atual->prox))
-// 		return acc;
-// 	return (atual->vertice == atual->prox->vertice) ?
-// 				duplicados_aux(atual->prox, acc + 1) :
-// 				duplicados_aux(atual->prox, acc);
-// }
-
-
-// static Nodo *copia_lista(Nodo *head) {
-// 	if(!head)
-// 		return NULL;
-// 	Nodo *novo = malloc(sizeof(Nodo));
-// 	novo->vertice = head->vertice;
-// 	novo->prox = copia_lista(head->prox);
-// 	return novo;
-// }
 
 static uint duplicados_lista(Nodo *head, uint vertices, uint index, uint *lacos) {
 	uint *freq = calloc(vertices, sizeof(uint));
@@ -164,11 +107,3 @@ uint grau_minimo(Grafo *g) {
 	return menor;
 }
 
-// uint quantos_conexos(Grafo *g) {
-// 	int qtd = 0;
-// 	for(int i = 0; i < g->count; i++) {
-// 		if(g->array[i] != NULL)
-// 			qtd++;
-// 	}
-// 	return qtd;
-// }
